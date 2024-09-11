0ee1f41e380dd884ea80854662de561b79c45174
itz-marcus <marcusadonis5@gmail.com>
2024-09-11 16:58:26 +0200

    Upload

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,14 +1,44 @@
 <template>
     <div>
         <h1>Im the admin yo</h1>
-        <button class="button" id="noButton">click me for a FREE pair of shoes!!!!</button>
+        <button @mouseover="button()" class="button" id="noButton">click me for a FREE pair of shoes!!!!</button>
     </div>
 </template>
 <script>
+import Swal from 'sweetalert2'
 export default {
-    
+    methods: {
+        button(){
+            const yesButton = document.querySelector('#yesButton');
+            const noButton = document.querySelector('#noButton');
+
+            const moveNoButton = () => {
+            var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+            var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+
+            noButton.style.position = 'absolute';
+            noButton.style.left = `${x}px`;
+            noButton.style.top = `${y}px`;
+            }
+
+            const catchButton = () => {
+                Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"
+});
+            }
+
+
+            noButton.addEventListener('click',catchButton);
+            noButton.addEventListener('mouseenter', moveNoButton);
+        }
+    },
 }
+
 </script>
 <style>
-    
+  #noButton{
+    padding: 3vw;
+  }  
 </style>
